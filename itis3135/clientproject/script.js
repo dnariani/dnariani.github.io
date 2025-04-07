6d89f8484cd2e9a5ed887db738e834e5f130f3c4
Disha Nariani <dnariani@charlotte.edu>
2025-04-07 00:27:07 -0400

    code changes

--- itis3135/clientproject/script.js
+++ itis3135/clientproject/script.js
@@ -1,6 +1,6 @@
 // THIS IS USED THAT IF U CLICK THE CONTACT US BUTTON YOU BY DEFAULT GO TO MESSAGE FORM
 document.addEventListener("DOMContentLoaded", function () {
-    const contactButton = document.getElementById("contact-btn");
+    const contactButton = document.getElementById("contact");
     contactButton.addEventListener("click", function (contact) {
         contact.preventDefault(); 
         window.location.href = "messageform.html"; 
